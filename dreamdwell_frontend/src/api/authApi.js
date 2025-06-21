d8f4376fd36984ee3aa7fee4bde10368697e3ade
bhumi3292 <137974891+bhumi3292@users.noreply.github.com>
2025-06-21 13:14:28 +0545

    api frontend

--- dreamdwell_frontend/src/api/authApi.js
+++ dreamdwell_frontend/src/api/authApi.js
@@ -1,7 +1,17 @@
-import axios from "./api"
+import axios from "./api"; // Ensure this path correctly points to your axios instance
 
-export  const registerUserApi=(data) =>{
-    console.log(data)
-   return axios.post('/auth/register',data)
-}
-export  const loginUserApi=(data) => axios.post("/auth/login", data)
+export const registerUserApi = (data) => {
+    console.log(data);
+    return axios.post('/auth/register', data);
+};
+
+export const loginUserApi = (data) => {
+    return axios.post("/auth/login", data);
+};
+
+export const sendPasswordResetLinkApi = (data) => {
+    return axios.post('/auth/request-reset/send-link', data);
+};
+
+export const resetPasswordApi = async (data, token) => // Corrected token concatenation
+    axios.post(`/auth/reset-password/${token}`, data);
