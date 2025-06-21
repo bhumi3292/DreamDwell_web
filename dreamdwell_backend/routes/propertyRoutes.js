952914d0f956bd3a39152d672966d40953a6ed7d
bhumi3292 <137974891+bhumi3292@users.noreply.github.com>
2025-06-21 13:07:19 +0545

    property

--- dreamdwell_backend/routes/propertyRoutes.js
+++ dreamdwell_backend/routes/propertyRoutes.js
@@ -1,13 +1,54 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const propertyController = require("../controllers/propertyController");
-const { protect } = require("../middlewares/auth");
 
-// Create a property (protected, landlord only)
-router.post("/", protect, propertyController.createProperty);
+const {
+    createProperty,
+    getAllProperties,
+    getOneProperty,
+    updateProperty,
+    deleteProperty
+} = require('../controllers/property/propertyController');
+
+const {
+    authenticateUser,
+    isLandlord,
+    isPropertyOwner
+} = require('../middlewares/authorizedUser');
+
+const { uploadPropertyMedia } = require('../middlewares/property/propertyMediaUpload');
+
+// Create Property (Landlord only)
+router.post(
+    '/',
+    authenticateUser,
+    isLandlord,
+    uploadPropertyMedia,
+    createProperty
+);
 
 // Get all properties (public)
-router.get("/", propertyController.getAllProperties);
+router.get('/', getAllProperties);
 
-// Export router
+// Get single property (public)
+router.get('/:id', getOneProperty);
+
+// Update property (Landlord + owner)
+router.put(
+    '/:id',
+    authenticateUser,
+    isLandlord,
+    isPropertyOwner,
+    uploadPropertyMedia,
+    updateProperty
+);
+
+// Delete property (Landlord + owner)
+router.delete(
+    '/:id',
+    authenticateUser,
+    isLandlord,
+    isPropertyOwner,
+    deleteProperty
+);
+
 module.exports = router;
