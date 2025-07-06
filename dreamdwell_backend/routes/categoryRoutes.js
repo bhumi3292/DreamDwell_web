984da98d5e06910a1b62c886f5d0d68460e943d5
bhumi3292 <137974891+bhumi3292@users.noreply.github.com>
2025-07-06 15:35:26 +0545

    category

--- dreamdwell_backend/routes/categoryRoutes.js
+++ dreamdwell_backend/routes/categoryRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const {
     createCategory,
     getCategories,
@@ -8,15 +7,10 @@ const {
     updateCategory,
     deleteCategory
 } = require('../controllers/category/categoryController');
-
 const { authenticateUser } = require('../middlewares/authorizedUser');
-// If you have an isAdmin middleware, use it like: authenticateUser, isAdmin
 
-// Public routes
 router.get('/', getCategories);
 router.get('/:id', getCategoryById);
-
-// Protected routes (consider adding isAdmin if needed)
 router.post('/', authenticateUser, createCategory);
 router.put('/:id', authenticateUser, updateCategory);
 router.delete('/:id', authenticateUser, deleteCategory);
