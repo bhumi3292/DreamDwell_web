35d1c80c14ff2a585e81bd3c3cb043f30f416a06
bhumi3292 <137974891+bhumi3292@users.noreply.github.com>
2025-07-06 15:34:54 +0545

    booking route update

--- dreamdwell_backend/routes/bookingRoutes.js
+++ dreamdwell_backend/routes/bookingRoutes.js
@@ -12,14 +12,10 @@ const { protect } = require("../middlewares/auth");
 const roleCheck = require("../middlewares/role");
 
 const requireTenant = roleCheck("Tenant");
-// const requireLandlord = roleCheck("Landlord"); // Uncomment when needed
 
 // Routes
 router.post("/create", protect, requireTenant, createBooking);
 router.get("/tenant", protect, requireTenant, getMyBookings);
 
-// Uncomment when implemented
-// router.get("/property/:propertyId", authenticateUser, requireLandlord, getBookingsForProperty);
-// router.delete("/:id", authenticateUser, requireTenant, cancelBooking);
 
 module.exports = router;
