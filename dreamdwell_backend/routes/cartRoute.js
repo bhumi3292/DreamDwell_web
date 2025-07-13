03922cd4141633bb8d226d5e143334ff6138e91e
bhumi3292 <137974891+bhumi3292@users.noreply.github.com>
2025-07-13 20:32:25 +0545

    cart route update

--- dreamdwell_backend/routes/cartRoute.js
+++ dreamdwell_backend/routes/cartRoute.js
@@ -4,12 +4,10 @@ const cartController = require('../controllers/cartController');
 const { authenticateUser } = require('../middlewares/authorizedUser'); // Corrected path
 
 
-router.use(authenticateUser);
+router.use(authenticateUser); // Apply authentication to all cart routes
 router.get('/', cartController.getCart);
-router.post('/add', cartController.addToCart); // <--- This is POST /api/cart/add
-
+router.post('/add', cartController.addToCart);
 router.delete('/remove/:propertyId', cartController.removeFromCart);
-
 router.delete('/clear', cartController.clearCart);
 
-module.exports = router;
+module.exports = router; // FIX: Corrected 'Exports' to 'exports'
